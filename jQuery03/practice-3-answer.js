a79a386bddc50cb3aa2bd38ea6280db3e0a33f22
itagaki <itagaki.shintaro@gmail.com>
2014-08-23 11:26:19 +0900

    add answer

---
+++ jQuery03/practice-3-answer.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function(){
+	// th行及びtd行のクローンをそれぞれ用意
+	var title = $("tr:first").clone(); // 最初のtr(タイトル行)のクローン
+	var input = $("tr:first").next().clone(); // 最初のtrの次の兄弟要素(次の兄弟要素はtdを子に持つtr＝最初の入力行)のクローン
+	
+	// 最後尾に追加
+	$("#ap-add").click(function(){
+		$(input).clone().appendTo("table");
+	});
+	// 入力行の削除
+	$("#del").click(function(){
+		$("td").parent().remove(); // tdの親のtr(tdを子に持つtr要素＝入力行)を全て削除
+		$(input).clone().appendTo("table"); // tableに入力行を追加
+	});
+	// 先頭に追加
+	$("#pre-add").click(function(){
+		$("tr:first").remove(); // 最初のtr(thを持つ行)を削除;
+		$(input).clone().prependTo("table"); // tableの先頭に入力行を追加
+		$(title).clone().prependTo("table"); // tableの先頭にタイトル行を追加
+	});
+});
---
+++ jQuery05/lightsout-d3.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function(){
+	$(".light").css("display","none");
+	var start_flg;
+	$("#start").click(function(){
+		start_flg = 1;
+		$("#clear").text("");
+		$(this).prop("disabled", true);
+		$("#end").prop("disabled", false);
+	});
+	$("#end").click(function(){
+		start_flg = 0;
+		$(this).prop("disabled", true);
+		$("#start").prop("disabled", false);
+	});
+
+	$("td").click(function(){
+		if(/* クリックされたtdの中のinputがチェックされていたら */){
+			// Q.そのinputのチェックを外す
+		}else{
+			// Q.そのinputのチェックを付ける
+		}
+		if(start_flg === 1){
+			var index = $("td").index(this);
+			// チェックボックスのみのライツアウトを作った時と同様に、隣接する<td>についても処理を記述
+			colorUpdate();
+			// 全てのマスの点灯判定を行う
+			var i = 0;
+			var flg = 1;
+			for(i = 0; i < 25; i++){
+				// チェックがついているものがあればクリアフラグを0(未クリア)にする
+				if($("input").eq(i).prop("checked")){
+					flg = 0;
+				}
+			}
+			// チェック後にクリアフラグが成立していたらクリアメッセージを表示
+			if(flg === 1){
+				$("#end").prop("disabled", true);
+				$("#start").prop("disabled", false);
+				start_flg = 0;
+				alert("Congratulations!!");
+			}
+		}
+		colorUpdate();
+	});
+});
